fd9634e7e733665e9f3b24de76a245ec1faadc43
Mahmoud Adas <mido3ds@gmail.com>
2021-05-31 15:57:54 +0200

    refactor

---
+++ src/symbols_test.cpp
@@ -0,0 +1,17 @@
+#include "symbols.h"
+#include <iostream>
+
+#ifdef TEST_SYMBOLS
+int main() {
+    Symbol *s =new Symbol {"aa"};
+    SymbolTable table;
+
+    table.add(s);
+    table.add(s);
+    Symbol* a ;
+    a = table.get((*s).name);
+    std::cout<<(*a).name<<std::endl;
+
+    return 0;
+}
+#endif
