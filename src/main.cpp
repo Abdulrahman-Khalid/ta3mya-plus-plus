6091a2e0f11c8cb46fdb8806300b8b1241949dae
Abdulrahman Khalid <abdulrahman.elshafei98@gmail.com>
2021-06-06 02:18:34 +0200

    Quadruple and QuadruplesTable finished

--- src/main.cpp
+++ src/main.cpp
@@ -12,24 +12,24 @@ inline void usage(char* p) {
 }
 
 int main(int argc, char **argv) {
-    if (argc == 1) {
-        yyin = stdin;
-    } else if (argc == 2) {
-        const auto s = std::string(argv[1]);
-        if (s == "-h" || s == "--help") {
-            usage(argv[0]);
-            return 0;
-        } else {
-            yyin = fopen(argv[1], "r");
-            if (yyin == NULL) {
-                std::cerr << "Invalid file path: " << argv[1] << std::endl;
-                return 1;
-            }
-        }
-    } else {
-        usage(argv[0]);
-        return 1;
-    }
+	if (argc == 1) {
+		yyin = stdin;
+	} else if (argc == 2) {
+		const auto s = std::string(argv[1]);
+		if (s == "-h" || s == "--help") {
+			usage(argv[0]);
+			return 0;
+		} else {
+			yyin = fopen(argv[1], "r");
+			if (yyin == NULL) {
+				std::cerr << "Invalid file path: " << argv[1] << std::endl;
+				return 1;
+			}
+		}
+	} else {
+		usage(argv[0]);
+		return 1;
+	}
 
 #ifdef DEBUG_LEX
     std::cout << "enabled only-lexical-analysis mode" << std::endl;
@@ -42,25 +42,25 @@ int main(int argc, char **argv) {
     yydebug = 1;
 #endif
 
-    int yyparse_return = yyparse();
-    if (yyparse_return !=0)
-    {
-    return yyparse_return;
-    }
+	int yyparse_return = yyparse();
+	if (yyparse_return !=0)
+	{
+		return yyparse_return;
+	}
 
-    CompileContext compile_context;// = new CompileContext() ;
+	CompileContext compile_context;// = new CompileContext() ;
 
-    // compile return  Program :std::vector<AssemblyLine> string
-    prgnodeptr->compile(compile_context);
+	// compile return  Program :std::vector<AssemblyLine> string
+	prgnodeptr->compile(compile_context);
 
-    Program p = compile_context.toProgram();
-    for (const auto& line : p)
-    {
-        std::cout << line << std::endl;
-    }
+	Program p = compile_context.toProgram();
+	for (const auto& line : p)
+	{
+		std::cout << line << std::endl;
+	}
 
-    std::cout<<endl;
+	std::cout<<endl;
 
-    return 0;
+	return 0;
 
 }
--- src/node.h
+++ src/node.h
@@ -3,30 +3,36 @@
 #include "scope.h"
 #include "symbols.h"
 #include "quadraples.h"
-#include <cassert>
 using std::string;
 class CompileContext
 {
-  public:
-    ScopeTracker scope_tracker;
-    SymbolTable  symbol_table;
-    QuadruplesTable quadruples_table;
-    Program toProgram() const {
-      Program p;
-      for (auto &quadrauple : quadruples_table)
-      {
-        assert(quadrauple.size() == 4);
-        AssemblyLine line = NULL; //TODO
-        p.push_back(line);
-      }
-      return p;
-    }
+	public:
+		ScopeTracker scope_tracker;
+		SymbolTable  symbol_table;
+		QuadruplesTable quadruples_table;
+
+		Program toProgram() const {
+			Program p;
+			for (auto &quadrauple : quadruples_table)
+			{
+				AssemblyLine line = "";
+				for (uint8_t i = 0; i < quadrauple.size(); i++)
+				{
+					if (quadrauple[i].size() > 0)
+						line += quadrauple[i];
+					if (quadrauple[i].size()-1 != i)
+						line += " ";
+				}
+				p.push_back(line);
+			}
+			return p;
+		}
 };
 class Node {
 public:
-    virtual void compile(CompileContext & compile_context ) const = 0;
+	virtual void compile(CompileContext & compile_context ) const = 0;
 
-    // toString returns a string representation
-    // of the object for debugging
-    virtual string toString() const = 0;
+	// toString returns a string representation
+	// of the object for debugging
+	virtual string toString() const = 0;
 };
