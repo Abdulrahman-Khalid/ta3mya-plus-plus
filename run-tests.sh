a88b1362ee1a5f6ed85103b77afb09fecd9ed96e
Mahmoud Adas <mido3ds@gmail.com>
2021-06-08 19:29:04 +0200

    failure ta -> stderr

--- run-tests.sh
+++ run-tests.sh
@@ -3,4 +3,4 @@
 find tests/should-pass/ | grep '\.ta$' | xargs -I{} bash -c "./src/ta3myac --only-symbol-table {} >{}sm 2>{}.symtable"
 find tests/should-pass/ | grep '\.ta$' | xargs -I{} bash -c "./src/ta3myac {} >{}sm 2>/dev/null"
 
-find tests/should-fail/ | grep '\.ta$' | xargs -I{} bash -c "./src/ta3myac {} 2>{}sm"
+find tests/should-fail/ | grep '\.ta$' | xargs -I{} bash -c "./src/ta3myac {} 2>{}.stderr"
