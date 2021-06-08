8657bd9e5e7c82211aa3cb02e644eb78549bc6cc
Mahmoud Adas <mido3ds@gmail.com>
2021-06-08 06:16:40 +0200

    fixed script

--- run-tests.sh
+++ run-tests.sh
@@ -1,3 +1,6 @@
 #!/bin/bash -e
+
 find tests/should-pass/ | grep '\.ta3$' | xargs -I{} bash -c "./src/ta3myac --only-symbol-table {} >{}.output 2>{}.symtable"
+find tests/should-pass/ | grep '\.ta3$' | xargs -I{} bash -c "./src/ta3myac {} >{}.output 2>/dev/null"
+
 find tests/should-fail/ | grep '\.ta3$' | xargs -I{} bash -c "./src/ta3myac {} 2>{}.output"
